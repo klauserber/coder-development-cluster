8dcd03b2e591db01a316b678095b0ef50b63ff76
Klaus Erber <k.erber@isium.de>
2022-10-15 12:25:37 +0000

    testing run pipeline (wip)

--- run_container.sh
+++ run_container.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-./build.sh
+# ./build.sh
 
 . ./VERSION
 
 ENTRYPOINT=${1:-/app/run.sh}
 
-docker run -it -v $(pwd)/config:/app/config --rm --entrypoint=${ENTRYPOINT} ${REGISTRY_NAME}/${IMAGE_NAME}:latest
+docker run -it \
+  -v $(pwd)/config:/app/config \
+  --rm \
+  --entrypoint=${ENTRYPOINT} \
+  ${REGISTRY_NAME}/${IMAGE_NAME}:latest
