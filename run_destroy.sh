e8019f5e45f23c6816757776f108b4f0d1415f89
Klaus Erber <k.erber@isium.de>
2022-10-22 15:46:31 +0000

    destroy playbook conditional in workflow

--- run_destroy.sh
+++ run_destroy.sh
@@ -5,15 +5,20 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 GCLOUD_PROJECT=${1:-${GCLOUD_PROJECT}}
 CLUSTER_NAME=${2:-${CLUSTER_NAME}}
+UNINSTALL_APPS=${3:-true}
 
 if [ -z "${CLUSTER_NAME}" ] || [ -z "${GCLOUD_PROJECT}" ]; then
-  echo "Usage: $0 <GCLOUD_PROJECT> <CLUSTER_NAME>"
+  echo "Usage: $0 <GCLOUD_PROJECT> <CLUSTER_NAME> [UNINSTALL_APPS 'true' or 'false' default 'true']"
   exit 1
 fi
 
 ${SCRIPT_DIR}/activate_service_account.sh ${GCLOUD_PROJECT}
 
 ${SCRIPT_DIR}/secrets_get.sh ${CLUSTER_NAME}
+
+if [[ ! ${UNINSTALL_APPS} == "false" ]]; then
+  ansible-playbook -i inventory ${SCRIPT_DIR}/automate/destroy.yml
+fi
 
 ansible-playbook -i inventory ${SCRIPT_DIR}/automate/tf_vars.yml
 
