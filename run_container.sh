bcfb6e74d11b67db5e6337a8bc1f13a889917ff3
Klaus Erber <k.erber@isium.de>
2022-12-06 09:07:55 +0000

    fix: always pull on run_container

--- run_container.sh
+++ run_container.sh
@@ -2,25 +2,29 @@
 
 REGISTRY_NAME=isi006
 IMAGE_NAME=coder-development-cluster
+TAG=latest
+
+echo "pull image ${REGISTRY_NAME}/${IMAGE_NAME}:${TAG}"
+docker pull ${REGISTRY_NAME}/${IMAGE_NAME}:${TAG} > /dev/null
 
 COMMAND=${1}
 
-if [[ ! ${COMMAND} =~ ^(run_create|run_destroy|secrets_get|secrets_create|secrets_delete|get_kubeconfig)$ ]]; then
+if [[ ! ${COMMAND} =~ ^(run_create|run_destroy|secrets_get|secrets_create|secrets_delete)$ ]]; then
   echo "Usage: $0 COMMAND [google-project-name] [cluster-name]"
   echo ""
   echo "  COMMANDS:"
   echo "    run_create - create a cluster"
-  echo "    run_destroy - create a cluster"
+  echo "    run_destroy - destroy a cluster"
   echo "    secrets_get - get the secrets from the google secret of a cluster"
   echo "    secrets_create - write the secrets to the google secret of a cluster"
   echo "    secrets_delete - delete the google secret of a cluster"
-  echo "    get_kubeconfig - get the kubeconfig of a cluster"
   echo ""
   exit 1
 fi
+
 
 docker run -it \
   -v $(pwd)/config:/app/config \
   --rm \
   --entrypoint=/app/${COMMAND}.sh \
-  ${REGISTRY_NAME}/${IMAGE_NAME}:latest ${@:2}
+  ${REGISTRY_NAME}/${IMAGE_NAME}:${TAG} ${@:2}
