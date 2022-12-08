6ffa4cce3c6376b1a220041fdbbdd0c709633752
Klaus Erber <k.erber@isium.de>
2022-12-08 21:18:10 +0000

    fix: better method to remove all apps

--- automate/roles/coder/coder_templates/devbox/main.tf
+++ automate/roles/coder/coder_templates/devbox/main.tf
@@ -21,6 +21,9 @@ resource "coder_agent" "devbox" {
 resource "kubernetes_namespace" "work-ns" {
   metadata {
     name = "${var.workspaces_namespace}-${local.workspace_owner}-${local.workspace_name}"
+    labels = {
+      "user-namespace" = "1"
+    }
   }
 }
 
