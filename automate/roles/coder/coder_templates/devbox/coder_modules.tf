8cdcbed0072d9cbfc77e9ed2b25eb0ca72b683f4
Klaus Erber <k.erber@isium.de>
2023-12-08 08:53:39 +0000

    fix: Remove jetbrains_gateway module

--- automate/roles/coder/coder_templates/devbox/coder_modules.tf
+++ automate/roles/coder/coder_templates/devbox/coder_modules.tf
@@ -5,12 +5,3 @@ module "filebrowser" {
   folder        = "/home/coder"
   database_path = ".config/filebrowser.db"
 }
-
-module "jetbrains_gateway" {
-  count          = var.jetbrains_module ? 1 : 0
-  source         = "https://registry.coder.com/modules/jetbrains-gateway"
-  agent_id       = coder_agent.devbox.id
-  agent_name     = "main"
-  folder         = "/home/coder/workspace/app"
-  jetbrains_ides = ["IU", "GO", "WS", "PY", "PC", "PS", "CL", "RM", "DB", "RD"]
-}
